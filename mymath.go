8f4ca411ed8515674061aa7ad1398a2399e40db1
wisphes <wisphes@gmail.com>
2023-09-10 18:59:20 +0300

    commit

--- mymath.go
+++ mymath.go
@@ -2,12 +2,12 @@ package mymath
 
 import "math"
 
-func Sqrt(x float64) float64 {
-	return math.Sqrt(x)
-}
-
 func Abs(x float64) float64 {
 	return math.Abs(x)
+}
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
 }
 
 func Max(x, y float64) float64 {
